29d6b3210047d021f508b3d11b45745536fdd2d5
mark.guo <mark.guo@hk.ncs-i.com>
2022-09-20 18:13:11 +0800

    3

--- 3.longestSubstring.cs
+++ 3.longestSubstring.cs
@@ -1,26 +1,17 @@
-public class Solution
-{
-    public int LengthOfLongestSubstring(string s)
-    {
-        if (string.IsNullOrEmpty(s))
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        if(string.IsNullOrEmpty(s))
             return 0;
         var subset = new List<char>();
         var result = 0;
-        for (var i = 0; i < s.Length; i++)
-        {
-            if (subset.Contains(s[i]))
-            {
+        for(var i = 0; i< s.Length; i++){
+            if(subset.Contains(s[i])){
                 result = Math.Max(result, subset.Count);
                 // need to remove all items at this duplicated point
-                var index = subset.IndexOf(s[i]);
-                subset.RemoveRange(0, index + 1);
-            }
-            else
-            {
-                result++;
+                subset.RemoveRange(0, subset.IndexOf(s[i])+1);
             }
             subset.Add(s[i]);
         }
-        return result;
+        return Math.Max(result, subset.Count);
     }
 }
